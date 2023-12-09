cf8bb7f9c8ac2d3444f51abf62d795ef02cf2104
ArfainSaba <89732721+ArfainSaba@users.noreply.github.com>
2023-12-09 11:58:42 +0530

    files added

---
+++ data/blogs.js
@@ -0,0 +1,24 @@
+const blogs = [
+  {
+    title: "this is blog 1",
+    content: " blog 1 has content related to blog 1",
+    slug: "post-1",
+  },
+  {
+    title: "this is blog 2",
+    content: " blog 1 has content related to blog 2",
+    slug: "post-2",
+  },
+  {
+    title: "this is blog 3",
+    content: " blog 1 has content related to blog 3",
+    slug: "post-3",
+  },
+  {
+    title: "this is blog 4",
+    content: " blog 1 has content related to blog 4",
+    slug: "post-4",
+  },
+];
+
+module.exports = blogs;
